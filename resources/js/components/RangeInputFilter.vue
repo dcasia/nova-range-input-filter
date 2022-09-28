d38512fd514edc4628d72a8157a4b342f1e08d00
C-Bass <vesperknight@gmail.com>
2022-09-28 17:14:45 +0300

    Nova 4 Compatible (#29)

--- resources/js/components/RangeInputFilter.vue
+++ resources/js/components/RangeInputFilter.vue
@@ -1,84 +1,73 @@
 <template>
 
-    <div class="range-input-filter">
+  <div class="range-input-filter pt-2 pb-3">
 
-        <h3 class="text-sm uppercase tracking-wide text-80 bg-30 p-3">
-            {{ filter.name }}
-        </h3>
+    <h3 class="px-3 text-xs uppercase font-bold tracking-wide">
+      {{ filter.name }}
+    </h3>
 
-        <div class="flex p-2 flex-no-wrap items-center">
+    <div class="flex p-2 flex-no-wrap items-center">
 
-            <input class="flex items-center form-control form-input text-sm border-60"
-                   :class="{ 'w-full': filter.options.fullWidth }"
-                   name="from"
-                   :type="filter.options.inputType"
-                   :placeholder="filter.options.fromPlaceholder"
-                   v-model="value.from"
-                   @change="handleChange"/>
+      <input class="w-full block form-control form-select form-control-sm form-select-bordered"
+             :class="{ 'w-full': filter.options.fullWidth }"
+             name="from"
+             :type="filter.options.inputType"
+             :placeholder="filter.options.fromPlaceholder"
+             v-model="value.from"
+             @change="handleChange"/>
 
-            <div class="text-sm mx-2 text-center">{{ filter.options.dividerLabel }}</div>
+      <div class="text-sm mx-2 text-center">{{ filter.options.dividerLabel }}</div>
 
-            <input class="flex items-center form-control form-input text-sm border-60"
-                   :class="{ 'w-full': filter.options.fullWidth }"
-                   name="to"
-                   :type="filter.options.inputType"
-                   :placeholder="filter.options.toPlaceholder"
-                   v-model="value.to"
-                   @change="handleChange"/>
-
-        </div>
+      <input class="w-full block form-control form-select form-control-sm form-select-bordered"
+             :class="{ 'w-full': filter.options.fullWidth }"
+             name="to"
+             :type="filter.options.inputType"
+             :placeholder="filter.options.toPlaceholder"
+             v-model="value.to"
+             @change="handleChange"/>
 
     </div>
+
+  </div>
 
 </template>
 
 <script>
-
-    export default {
-        name: 'RangeInputFilter',
-        props: {
-            resourceName: {
-                type: String,
-                required: true
-            },
-            filterKey: {
-                type: String,
-                required: true
-            }
-        },
-        methods: {
-            handleChange(event) {
-
-                /**
-                 * Dont refresh the page if user is still typing the values
-                 */
-                if (Number.isNaN(parseInt(this.value.from)) !==
-                    Number.isNaN(parseInt(this.value.to))) {
-
-                    return
-
-                }
-
-                this.$store.commit(`${ this.resourceName }/updateFilterState`, {
-                    filterClass: this.filterKey,
-                    value: {
-                        ...this.value,
-                        [ event.target.name ]: event.target.value
-                    }
-                })
-
-                this.$emit('change')
-
-            }
-        },
-        computed: {
-            filter() {
-                return this.$store.getters[ `${ this.resourceName }/getFilter` ](this.filterKey)
-            },
-            value() {
-                return { ...this.filter.currentValue } || { from: '', to: '' }
-            }
-        }
+export default {
+  name: 'RangeInputFilter',
+  props: {
+    resourceName: {
+      type: String,
+      required: true
+    },
+    filterKey: {
+      type: String,
+      required: true
     }
+  },
+  methods: {
+    handleChange(event) {
+
+      this.$store.commit(`${ this.resourceName }/updateFilterState`, {
+        filterClass: this.filterKey,
+        value: {
+          ...this.value,
+          [ event.target.name ]: event.target.value
+        }
+      })
+
+      this.$emit('change')
+
+    }
+  },
+  computed: {
+    filter() {
+      return this.$store.getters[ `${ this.resourceName }/getFilter` ](this.filterKey)
+    },
+    value() {
+      return { ...this.filter.currentValue } || { from: '', to: '' }
+    }
+  }
+}
 
 </script>
